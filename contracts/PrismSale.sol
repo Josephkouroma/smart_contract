3b5f5f3942f51513e5ea6f26ac2d625a3836ccdd
Joseph Kouroma <jkouroma@gmail.com>
2022-05-11 21:09:55 +0100

    adding publc addressess

--- contracts/PrismSale.sol
+++ contracts/PrismSale.sol
@@ -6,9 +6,15 @@ contract PrismSale {
   uint public totalSales;
   uint public maxSales;
 
+  address public owner;
+  address public charity;
+
   constructor() {
     totalSales = 0;
     maxSales = 100;
+
+    owner = 0x2bEfe08D9143C6b39C71cD98a546AFce8c0868e5;
+    charity = 0x0Abf11af8DeD7019206BC46E3B4A5569103D0a15;
 
   }
 }
