a94f7125278e6cd7d4ccae997087e795851d9f9f
Joseph Kouroma <jkouroma@gmail.com>
2022-05-19 10:35:02 +0100

    Does the user already have access the album

--- contracts/PrismSale.sol
+++ contracts/PrismSale.sol
@@ -9,6 +9,8 @@ contract PrismSale {
   address public owner;
   address public charity;
 
+  mapping (address => bool) sales;
+
   constructor() {
     totalSales = 0;
     maxSales = 100;
@@ -22,14 +24,22 @@ contract PrismSale {
     return totalSales < maxSales;
   }
 
+  function hasAccess () public returns (bool) {
+    return sales[msg.sender];
+  }
+
   function buy () public payable returns (bool) {
     require(canBuy() == true, "can't buy this");
     require(msg.value ==  0.01 ether, "you did not send the correct ammout");
+    require(hasAccess() == false, "already bought");
 
     payable(owner).transfer(msg.value * 80 / 100);
     payable(charity).transfer(msg.value * 20 / 100);
 
     totalSales = totalSales +1;
+
+    sales[msg.sender] = true;
+  
     return true;
   }
 }
