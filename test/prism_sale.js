091acc1049f01c0fe6f7488990776e25b98962db
Joseph Kouroma <jkouroma@gmail.com>
2022-05-21 15:05:00 +0100

    Testing whether the owner and charity addresses are correct

--- test/prism_sale.js
+++ test/prism_sale.js
@@ -5,9 +5,19 @@ const PrismSale = artifacts.require("PrismSale");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("PrismSale", function (/* accounts */) {
-  it("should assert true", async function () {
+contract('PrismSale', function (accounts) {
+  it('should assert true', async function () {
     await PrismSale.deployed();
     return assert.isTrue(true);
+  });
+
+  it('should get the right account', async function () {
+    const contract = await PrismSale.deployed();
+
+    const owner = await contract.owner.call();
+    const charity = await contract.charity.call();
+
+    assert.isTrue(owner == 0x2befe08d9143c6b39c71cd98a546afce8c0868e5);
+    assert.isTrue(charity == 0x0abf11af8ded7019206bc46e3b4a5569103d0a15);
   });
 });
