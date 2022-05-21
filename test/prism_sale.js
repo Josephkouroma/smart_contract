d92d87b00cb5526cf037094bcfd320926b4dcdd6
Joseph Kouroma <jkouroma@gmail.com>
2022-05-21 15:53:10 +0100

    Testing whether payments get split between owner and charity

--- test/prism_sale.js
+++ test/prism_sale.js
@@ -20,4 +20,47 @@ contract('PrismSale', function (accounts) {
     assert.isTrue(owner == 0x2befe08d9143c6b39c71cd98a546afce8c0868e5);
     assert.isTrue(charity == 0x0abf11af8ded7019206bc46e3b4a5569103d0a15);
   });
+
+  it('it should split the payment', async function () {
+    const contract = await PrismSale.deployed();
+
+    const startBalance = web3.utils.toBN(
+      await web3.eth.getBalance(accounts[2])
+    );
+
+    const purchase = await contract.buy.sendTransaction({
+      from: accounts[0],
+      value: web3.utils.toWei('0.01', 'ether'),
+    });
+
+    const commission = web3.utils.toBN(web3.utils.toWei('0.008', 'ether'));
+    const endBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[2]));
+
+    assert.equal(
+      startBalance.add(commission).toString(),
+      endBalance.toString()
+    );
+  });
+
+  it('it should split the payment to the charity', async function () {
+    const contract = await PrismSale.deployed();
+
+    const startBalance = web3.utils.toBN(
+      await web3.eth.getBalance(accounts[3])
+    );
+
+    const purchase = await contract.buy.sendTransaction({
+      from: accounts[5],
+      value: web3.utils.toWei('0.01', 'ether'),
+    });
+
+    const commission = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
+    const endBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[3]));
+
+    assert.equal(
+      startBalance.add(commission).toString(),
+      endBalance.toString()
+    );
+  });
+
 });
